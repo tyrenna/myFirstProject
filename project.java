0bb63041291269ed9fd9ca4bbb11485cfd879f06
Tyrenna Gaines <tyrennagaines@gmail.com>
2022-06-29 20:12:10 -0400

    first commit

---
+++ project.java
@@ -0,0 +1,11 @@
+package myFirstProject;
+
+public class project {
+
+	public static void main(String[] args) {
+	int age = 1;
+	System.out.println(age);
+
+	}
+
+}
---
+++ test.java
@@ -0,0 +1,11 @@
+package myFirstProject;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//test
+		int age = 1;
+	}
+
+}
